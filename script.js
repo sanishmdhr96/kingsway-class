043143c92f27351dc426760c956270d06ce96b76
sanishmdhr96 <mail.sanishmanandhar@gmail.com>
2024-03-21 10:12:43 +0545

    switch case statement

---
+++ script.js
@@ -0,0 +1,28 @@
+/**
+ * Switch case
+ */
+let dataPack = prompt('Which data pack would you like to use')
+
+switch (dataPack) {
+    //if(dataPack === "Unlimited 5G")
+    case 1:
+        alert("You have received unlimited 5g data for 1 day");
+        return;
+
+    case "Student Pack":
+        alert("You have received student pack for 1 day");
+        break;
+
+    case "Summer Pack":
+        alert("You have received summer pack for 1 day");
+        break;
+
+    case "Lockdown Pack":
+        alert("You have received lockdown pack for 1 day");
+        break;
+
+    default:
+        alert("Sorry, We donot have your requested datapack,please choose another datapack to continue");
+}
+
+
